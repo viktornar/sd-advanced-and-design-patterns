1e0eb60433d4467536262ed80a6783827d68eb76
Viktor Nareiko <vnareiko.lt@gmail.com>
2021-01-16 10:31:29 +0200

    Removed not needed files

--- app/src/main/java/com/sd/advanced/coding/examples/ExampleEncapsulation.java
+++ app/src/main/java/com/sd/advanced/coding/examples/ExampleEncapsulation.java
@@ -4,5 +4,30 @@ public class ExampleEncapsulation implements Example {
     @Override
     public void run() throws InterruptedException {
         System.out.println("Running from encapsulation");
+        Person p = new Person();
+
+        p.setName("ssss");
+        p.getName();
+    }
+
+    private class Person {
+        private String name;
+        private String surname;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getSurname() {
+            return surname;
+        }
+
+        public void setSurname(String surname) {
+            this.surname = surname;
+        }
     }
 }
